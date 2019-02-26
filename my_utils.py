f1df2289780ade88382c218f03c520ed5a35a771
Simon Grützner <sg@math.uni-bremen.de>
2019-02-26 23:29:12 +0100

    Playing around with dataframes.

--- my_utils.py
+++ my_utils.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
 
 PATH = 'dataset/'                                                                                   # set path variable for dataset
@@ -6,4 +6,12 @@ PATH = 'dataset/'                                                               
 table_names = ['train', 'store', 'store_states', 'state_names', 'googletrend', 'weather', 'test']   # input table names
 tables = [pd.read_csv(f'{PATH}{fname}.csv', low_memory=False) for fname in table_names]             # create list of DataFrames
 
-for t in tables: print(t.head())                                                                    # show first 5 lines of all data frames
+for t in tables:
+    # print(t.head())                                                                                 # show first 5 lines of all data frames
+    df = t.select_dtypes(include='number')
+    df = df.head()
+    # df = df.describe()
+    df = df.mean(axis=1, )
+    print(df)
+
+    quit()
